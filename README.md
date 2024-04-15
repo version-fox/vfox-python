208b5cd3f1e114209adc4713684fae7ec11ce5ba
lihan <lihan@apache.org>
2024-04-15 09:04:32 +0800

    release v0.1.2

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ After installing [vfox](https://github.com/version-fox/vfox), install the plugin
 ```bash
 vfox add python
 ```
+
+## Mirror
+
+You can configure the mirror by `VFOX_PYTHON_MIRROR` environment variable. The default value
+is `https://www.python.org/ftp/python/`.
--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "python"
 --- Plugin version
-PLUGIN.version = "0.1.1"
+PLUGIN.version = "0.1.2"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-python"
 --- Plugin license, please choose a correct license according to your needs.
