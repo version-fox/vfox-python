cbbf7534abc79cd5530a137649dac151478c48e8
lihan <lihan@apache.org>
2024-04-25 10:02:41 +0800

    release v0.2.0

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "python"
 --- Plugin version
-PLUGIN.version = "0.1.2"
+PLUGIN.version = "0.2.0"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-python"
 --- Plugin license, please choose a correct license according to your needs.
