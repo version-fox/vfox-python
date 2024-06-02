c969823de50e7fb43c114abed133ca963aba0c3a
janbar <janbar@qq.com>
2024-06-02 21:13:13 +0800

    release v0.3.1

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "python"
 --- Plugin version
-PLUGIN.version = "0.3.0"
+PLUGIN.version = "0.3.1"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-python"
 --- Plugin license, please choose a correct license according to your needs.
